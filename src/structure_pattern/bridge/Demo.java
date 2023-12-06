5573c324de0b8bc9c1ea53f7ccaeab3ccab8df24
김민수 MINSOO KIM <alstn113@gmail.com>
2023-12-07 01:42:51 +0900

    feat: bridge pattern

---
+++ src/structure_pattern/bridge/Demo.java
@@ -0,0 +1,27 @@
+package structure_pattern.bridge;
+
+import structure_pattern.bridge.devices.Device;
+import structure_pattern.bridge.devices.Radio;
+import structure_pattern.bridge.devices.Tv;
+import structure_pattern.bridge.remotes.AdvancedRemote;
+import structure_pattern.bridge.remotes.BasicRemote;
+
+public class Demo {
+    public static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
---
+++ src/structure_pattern/bridge/remotes/Remote.java
@@ -0,0 +1,13 @@
+package structure_pattern.bridge.remotes;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
