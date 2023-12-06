777ddbc1c0a684f62a9f852b4874d55eb30e58bc
김민수 MINSOO KIM <alstn113@gmail.com>
2023-12-07 00:22:23 +0900

    feat: abstract factory method

---
+++ src/abstract_factory/factories/GUIFactory.java
@@ -0,0 +1,13 @@
+package abstract_factory.factories;
+
+import abstract_factory.buttons.Button;
+import abstract_factory.checkboxes.Checkbox;
+
+/**
+ * Abstract factory knows about all (abstract) product types.
+ */
+public interface GUIFactory {
+    Button createButton();
+
+    Checkbox createCheckbox();
+}
