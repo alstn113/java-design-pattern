27b1e92d7ea5135268b6e8f7863e47766a111199
김민수 MINSOO KIM <alstn113@gmail.com>
2023-12-09 16:23:09 +0900

    feat: state pattern

---
+++ src/behavioral_pattern/state/Demo.java
@@ -0,0 +1,15 @@
+package behavioral_pattern.state;
+
+import behavioral_pattern.state.ui.Player;
+import behavioral_pattern.state.ui.UI;
+
+/**
+ * Demo class. Everything comes together here.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
