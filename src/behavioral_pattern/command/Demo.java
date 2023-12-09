4e91230ca51cf886cd9ca9cbbbfde02094e038b4
김민수 MINSOO KIM <alstn113@gmail.com>
2023-12-09 14:58:29 +0900

    feat: command pattern

---
+++ src/behavioral_pattern/command/Demo.java
@@ -0,0 +1,10 @@
+package behavioral_pattern.command;
+
+import behavioral_pattern.command.editor.Editor;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
---
+++ src/behavioral_pattern/command/commands/CopyCommand.java
@@ -0,0 +1,16 @@
+package behavioral_pattern.command.commands;
+
+import behavioral_pattern.command.editor.Editor;
+
+public class CopyCommand extends Command {
+
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
